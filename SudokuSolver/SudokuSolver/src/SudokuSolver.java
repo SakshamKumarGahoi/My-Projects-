b264f1f0576e5371852e08da8324ed6b520e7636
Saksham Kumar Gahoi <sakshamgahoi123@gmail.com>
2025-01-22 21:11:17 +0530

    sudoku trial

---
+++ SudokuSolver/SudokuSolver/src/SudokuSolver.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class SudokuSolver extends Application {
+    // Window dimensions
+    private static final int WINDOW_WIDTH = 800;
+    private static final int WINDOW_HEIGHT = 600;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Sudoku Solver");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        primaryStage.setTitle("Sudoku Solver");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
