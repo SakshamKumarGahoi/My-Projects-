4e4de28da6eff4a973a1e56a216cca1f88b64927
Saksham Kumar Gahoi <sakshamgahoi123@gmail.com>
2025-02-06 09:36:30 +0530

    sudoku solver
    sudoku solver

--- SudokuSolver/SudokuSolver/src/SudokuSolver.java
+++ SudokuSolver/SudokuSolver/src/SudokuSolver.java
@@ -5,14 +5,15 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class SudokuSolver extends Application {
-    // Window dimensions
-    private static final int WINDOW_WIDTH = 800;
-    private static final int WINDOW_HEIGHT = 600;
-    
+
+    private static final int WINDOW_WIDTH = 600;
+    private static final int WINDOW_HEIGHT = 400;
+
     @Override
     public void start(Stage primaryStage) {
         Label label = new Label("Sudoku Solver");
         StackPane root = new StackPane(label);
+
         Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
 
         primaryStage.setTitle("Sudoku Solver");
@@ -24,3 +25,4 @@ public class SudokuSolver extends Application {
         launch(args);
     }
 }
+
