f5e21f50eca419dfccfe6f763b883ca2aa12ac66
Saksham Kumar Gahoi <sakshamgahoi123@gmail.com>
2025-03-10 15:37:24 +0530

    File Sharing Platform
    working on a fsd personal project

---
+++ file-sharing-platform/file-sharing-platform/src/main/java/com/saksham/file_sharing_platform/FileSharingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.saksham.file_sharing_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSharingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSharingPlatformApplication.class, args);
+	}
+
+}
---
+++ file-sharing-platform/file-sharing-platform/src/test/java/com/saksham/file_sharing_platform/FileSharingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saksham.file_sharing_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSharingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
