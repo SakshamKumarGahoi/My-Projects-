a53810ca2daad88a4775fc04d33f44fee93e7233
Saksham Kumar Gahoi <sakshamgahoi123@gmail.com>
2025-03-18 19:28:51 +0530

    commit for new entities

--- FileSharingSystem/cloud-file-sharing/src/main/java/com/cloud/filesharing/entity/File.java
+++ FileSharingSystem/cloud-file-sharing/src/main/java/com/cloud/filesharing/entity/File.java
@@ -3,31 +3,42 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Date;
+import java.util.*;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
-@Table(name = "files")
+@Data
 public class File {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long fileId;
+    private Long Id;
 
-    @SuppressWarnings("unused")
-    private String fileName;
+    @Column(nullable = false,unique = true)
+    private String name;
+    
+    @Column(nullable = false)
+    private String type;
+    
+    @Column(nullable = false)
+    private String size;
 
-    @SuppressWarnings("unused")
-    private String fileType;
-
-    @Column(name = "upload_date")
+    @Column(nullable = false)
     private Date uploadDate;
     
     @ManyToOne
-    @JoinColumn(name = "user_id" , referencedColumnName = "id")
-    private User user;     
-
+    @JoinColumn(name = "uploaded_by" , nullable = false)
+    private User uploadedBy;
+    
+    @ManyToOne
+    @JoinTable(
+        name = "file_share",
+        joinColumns = @JoinColumn(name = "file_id"),
+        inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> sharedWith;
 }
--- FileSharingSystem/cloud-file-sharing/src/main/java/com/cloud/filesharing/entity/Share.java
+++ FileSharingSystem/cloud-file-sharing/src/main/java/com/cloud/filesharing/entity/Share.java
@@ -1,5 +1,85 @@
 package com.cloud.filesharing.entity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import org.springframework.cglib.core.Local;
+
+import java.io.Serializable;
+import java.util.*;
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+@Entity
+@Table(name = "share")
+@IdClass(Share.PK.class)
+@Data
 public class Share {
 
+    @EmbeddedId
+    private Share.PK pk;    
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "file_id", insertable = false, updatable = false)
+    private File file;
+
+    @Column(name = "shared_on", nullable = false)
+    private LocalDate sharedOn;
+    
+    @Column(name = "message", columnDefinition = "TEXT")
+    private String message;
+
+    @IdClass
+    public static class PK implements Serializable {
+        @Id
+        @Column(name = "user_id")
+        private Long userId;
+
+        @Id
+        @Column(name = "file_id")   
+        private Long fileId;
+
+        //NO ARG CONSTRUCTOR
+        public PK() {
+        }
+
+        public PK(Long userId, Long fileId) {
+            this.userId = userId;
+            this.fileId = fileId;
+        }
+
+        //GETTERS AND SETTERS
+
+        public Long getUserId() {
+            return userId;
+        }
+        public void setUserId(Long userId) {
+            this.userId = userId;
+        }
+        public Long getFileId() {
+            return fileId;
+        }
+        public void setFileId(Long fileId) {
+            this.fileId = fileId;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            PK pk = (PK) o;
+            return Objects.equals(userId, pk.userId) && Objects.equals(fileId , pk.fileId);
+        }
+    }
 }
